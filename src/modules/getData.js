0a64ea73bde82ccfb29ad3e71fe2579df5db3ada
Gayaz Urazov <gayazur@mail.ru>
2026-03-25 08:53:06 +0500

    lesson 2

---
+++ src/modules/getData.js
@@ -0,0 +1,8 @@
+const getData = () => {
+  return fetch('https://test-462a5-default-rtdb.firebaseio.com/goods.json')
+    .then((response) => {
+      return response.json();
+    });
+}
+
+export default getData;
